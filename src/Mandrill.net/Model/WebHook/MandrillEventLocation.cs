150c4e8e10dd05a885f6a49af7fc49c49277f761
feinoujc <feinoujc@users.noreply.github.com>
2015-08-01 15:06:12 -0400

    make properties nullable

--- src/Mandrill.net/Model/WebHook/MandrillEventLocation.cs
+++ src/Mandrill.net/Model/WebHook/MandrillEventLocation.cs
@@ -13,9 +13,9 @@ namespace Mandrill.Model
 
         public string City { get; set; }
 
-        public double Latitude { get; set; }
+        public double? Latitude { get; set; }
 
-        public double Longitude { get; set; }
+        public double? Longitude { get; set; }
 
         public string PostalCode { get; set; }
 
