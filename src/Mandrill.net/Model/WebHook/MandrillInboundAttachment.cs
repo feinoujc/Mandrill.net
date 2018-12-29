e803f1bb1ed4da80b0a113e1d44fa0c7e7bb9cf9
feinoujc <feinoujc@users.noreply.github.com>
2018-12-29 11:49:49 -0500

    add missing inbound properties
    resolves #81

---
+++ src/Mandrill.net/Model/WebHook/MandrillInboundAttachment.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Mandrill.Model
+{
+    public class MandrillInboundAttachment
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+        [JsonProperty("base64")]
+        public bool Base64 { get; set; }
+    }
+}
---
+++ src/Mandrill.net/Model/WebHook/MandrillInboundImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Mandrill.Model
+{
+    public class MandrillInboundImage
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
